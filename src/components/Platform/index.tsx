2514e2d9d5cb857198ba6ca1660df5e40faa1174
Arjun Mohan <arjunpotter17@gmail.com>
2024-12-20 06:24:27 +0530

    chore: removed carousel from platform

--- src/components/Platform/index.tsx
+++ src/components/Platform/index.tsx
@@ -1,4 +1,4 @@
-import Carousel from "../Projects/components/Carousel";
+// import Carousel from "../Projects/components/Carousel";
 import "../Projects/styles/carousel.css";
 
 
@@ -13,14 +13,14 @@ interface PlatformProps {
 }
 
 export default function Platform({
-  slideCount = 5, // Default value for slideCount
+  // slideCount = 5, // Default value for slideCount
   pre = "OUR PLATFORM", // Default value for title
   title = "Visionary Solutions", // Default value for subtitle
-  carouselOptions = { loop: true, dragFree: true }, // Default carousel options
+  // carouselOptions = { loop: true, dragFree: true }, // Default carousel options
 }: PlatformProps) {
   
   // Generate the slide items dynamically based on slideCount
-  const slides = Array.from(Array(slideCount).keys());
+  // const slides = Array.from(Array(slideCount).keys());
 
   return (
     <div className="flex flex-col w-full items-center gap-y-24">
@@ -30,9 +30,9 @@ export default function Platform({
       <p className="w-full text-center text-nilor-white text-landing-section-title">
         {title}
       </p>
-      <div className="flex overflow-x-auto px-4 max-w-[1152px] w-full gap-x-3 h-full">
+      {/* <div className="flex overflow-x-auto px-4 max-w-[1152px] w-full gap-x-3 h-full">
         <Carousel slides={slides} options={carouselOptions} />
-      </div>
+      </div> */}
     </div>
   );
 }

ac85562d90df0e6d88d99d878737182314037821
Stephen <>
2025-01-14 16:39:38 +0800

    Updated Tabs

--- src/app/constants/tabDetails.ts
+++ src/app/constants/tabDetails.ts
@@ -1,42 +1,35 @@
 import { TabDetails } from "@/lib/types";
 
 export const landingPageTabConstants = {
-  para: "All-encompassing software. Fundamental hardware. Complete support and learning services. A fully integrated ecosystem for seamlessly delivering complex graphics in real-time to any surface.",
-  title: "Innovative tools. Exceptional content. Unmatched expertise.",
+  pre: "we offer",
+  para: "Worldbuilding solutions. Wilderness exploration instruments. Idea unleashing services.",
 };
 
 export const productPageTabsConstants = {
-  pre: "We Deliver",
-  title: "Scalable and Versatile Products",
+  pre: "we deliver",
+  title: "Scalable, versatile products",
 };
 
 export const landingPageTabDetails: TabDetails[] = [
   {
-    tabName: "Visualise",
-    title: "Best Hardware 1",
+    tabName: "solutions",
+    title: "Worldbuilders",
     buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
+    para: "nilor takes pride in being able to offer end-to-end solutions. Whether you want people to cross over into another realm, or have your stories come to them in familiar spaces, nilor's team have years of experience in summoning worlds from mere pixels.",
     imgSrc: "/hardware-placeholder.webp",
   },
   {
-    tabName: "Design",
-    title: "Best Hardware 2",
+    tabName: "products",
+    title: "Toolmakers",
     buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
+    para: "nilor knows bleeding-edge, but it also knows robust. We love making instruments that help uncover connections between things. We make and use whatever is right for the job, promiscuously and without loyalties; we're about making things that are as exciting to use as the results they produce.",
     imgSrc: "/hardware-placeholder.webp",
   },
   {
-    tabName: "Control",
-    title: "Best Hardware 3",
+    tabName: "services",
+    title: "Shapeshifters",
     buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
-    imgSrc: "/hardware-placeholder.webp",
-  },
-  {
-    tabName: "Connect",
-    title: "Best Hardware 4",
-    buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
+    para: "nilor is a team of transdisciplinary humans who offer everything from creative direction to technical development. We can help you with everything from the initial concept to the final execution, from a little piece to the whole pie.",
     imgSrc: "/hardware-placeholder.webp",
   },
 ];
@@ -44,23 +37,23 @@ export const landingPageTabDetails: TabDetails[] = [
 export const productPageTabsDetails = [
   {
     tabName: "nilor.cool",
-    title: "Best Hardware 1",
+    title: "Rapid Evolution",
     buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
+    para: "Whether you need the back of the napkin sketch or the creative rabbithole, nilor.cool is the place to start. It's a platform that lets you explore, experiment, and refine your vision, together. It's designed for both technical and non-technical collaborators, and it transforms collective input into tangible, cohesive results.",
     imgSrc: "/hardware-placeholder.webp",
   },
   {
     tabName: "depth plus",
-    title: "Best Hardware 2",
+    title: "Critical Data",
     buttonText: "Learn More",
-    para: "This is a great hardware that we offer. It is obviously not as good as the others on the list. You should pick the one that is most expensive.",
+    para: "Extracting data as if from the depths of the ocean, Depth Plus takes your input videos and constructs their many parts, allowing you to see the unseen. Imagine pulling your videos apart, and then putting them back together with each other in new and exciting ways.",
     imgSrc: "/hardware-placeholder.webp",
   },
   {
     tabName: "zenerator",
-    title: "Empowering Creation",
+    title: "Zen Generation",
     buttonText: "Learn More",
-    para: "Unlock creative freedom with our cloud platform, featuring custom ComfyUI workflows in an intuitive GUI—no coding required. Effortlessly turn images into stunning videos and upscale them for high-resolution displays. Advanced users can even auto-generate GUIs for their own workflows. Create, iterate, and elevate with ease.",
+    para: "Unlock creative freedom with Zenerator, featuring custom workflows in an intuitive GUI—no coding required. Effortlessly turn images into stunning videos and upscale them for high-resolution displays. Advanced users can even auto-generate GUIs for their own workflows. Create, iterate, and elevate with ease.",
     imgSrc: "/hardware-placeholder.webp",
   },
 ];

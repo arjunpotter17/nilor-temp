1295d7ec13be226a542e81d7467eb3321eec95c5
Stephen <>
2025-01-14 17:49:29 +0800

    modified expDetails

--- src/app/constants/expDetails.ts
+++ src/app/constants/expDetails.ts
@@ -1,14 +1,14 @@
 export const ImmersiveExpData = {
   pre: "dynamic worlds",
   para: "nilor transforms spaces into dynamic, living worlds, delivering immersive visual experiences across the world’s most ambitious displays.",
-  title: "Venues are Ready for Cutting Edge AI",
+  title: "Venues are ready for cutting edge AI",
   buttonText: "Learn More",
   imgSrc: "/dynamic-worlds.png",
 };
 
 export const LiveEventsData = {
   pre: "live events",
-  title: "Reality Cubed",
+  title: "Reality cubed",
   para: "From intimate dining rooms to grand halls, our synchronized systems shape environments that captivate audiences and adapt seamlessly to their surroundings.",
   buttonText: "Learn More",
   imgSrc: "/reality-cubed.jpg",
@@ -16,16 +16,16 @@ export const LiveEventsData = {
 
 export const servicesTechnicalData = {
   pre: "Technical Services",
-  title: "Sidekicks and Superheroes",
-  para: "Whether you need us to own every stage of the pipeline, you want to work alongside us while we tackle specific elements, or you just need us to 'take care of' something, our team can handle it.",
+  title: "Superheroes and sidekicks",
+  para: "Whether you want us to own every stage of the pipeline, work alongside us while we tackle specific elements, or you just need us to 'take care of it', our team is ready to help.",
   buttonText: "Learn More",
   imgSrc: "/power_to_create.jpg",
 };
 
 export const servicesCreativeData = {
   pre: "Creative Services",
-  title: "Art in Motion",
-  para: "Our veteran VFX artists transform spaces through stunning visual narratives, from concept development to final execution. We craft custom content that pushes the boundaries of what's possible in immersive environments",
+  title: "Summoners and sorcerers",
+  para: "We can extract precious gems from the raw ore of your ideas, or we can teleport into your world and conjure up something entirely new. We can even do both.",
   buttonText: "Learn More",
   imgSrc: "/Art_in_motion.jpg",
 };

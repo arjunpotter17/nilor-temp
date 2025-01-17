bd87455d91906e85a8dc58255196ea4a4f924aeb
Stephen <>
2025-01-17 10:21:45 +0800

    modify text sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from "tailwindcss";
-import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
+import fluid, { extract, screens, fontSize } from "fluid-tailwind";
 
 export default {
-  content: {extract, files: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ]},
+  content: {
+    extract,
+    files: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+  },
   theme: {
     screens,
     fontSize,
@@ -14,27 +17,27 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        "nilor-black":"#000000",
-        "nilor-offwhite":"#f2f2f2",
-        "nilor-white":"#ffffff",
+        "nilor-black": "#000000",
+        "nilor-offwhite": "#f2f2f2",
+        "nilor-white": "#ffffff",
         "nilor-grey": "#D3D3D3",
-        "nilor-accent":"#F8607D",
-        "nilor-secondary":"#1d2939",
+        "nilor-accent": "#F8607D",
+        "nilor-secondary": "#1d2939",
       },
       boxShadow: {
-        'soft-glow': '0 4px 20px rgba(255, 255, 255, 0.5)', // Custom white glow shadow
+        "soft-glow": "0 4px 20px rgba(255, 255, 255, 0.5)", // Custom white glow shadow
       },
       fontFamily: {
-        sans: ["var(--font-nilor)", "sans-serif"], //overrides everywhere 
+        sans: ["var(--font-nilor)", "sans-serif"], //overrides everywhere
       },
       fontWeight: {
-        "nilor-light": '200',
-        "nilor-regular": '400',
-        "nilor-medium": '500',
-        "nilor-semibold": '600',
-        "nilor-bold": '700',
+        "nilor-light": "200",
+        "nilor-regular": "400",
+        "nilor-medium": "500",
+        "nilor-semibold": "600",
+        "nilor-bold": "700",
       },
-      fontSize:{
+      fontSize: {
         // "nilor-pre":['1.5rem', { lineHeight: '2.25rem' }],
         // "nilor-title":['4.5rem', { lineHeight: '1' }],
         // "nilor-section-title":['3rem', {lineHeight:'1.15rem'}],
@@ -43,18 +46,21 @@ export default {
         // "nilor-title-sm":['4rem',{lineHeight:'1'}],
         // "nilor-hero-text":['2.25rem', { lineHeight: '2.5rem' }],
         // "nilor-text-small":['1.75rem', {lineHeight: '2rem'}]
-
-      }
+      },
     },
   },
   plugins: [
-    fluid, 
+    fluid,
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     require("@xpd/tailwind-3dtransforms"),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    function (pluginApi: { addUtilities: (utilities: Record<string, any>) => void; }) {
+    function (pluginApi: {
+      addUtilities: (utilities: Record<string, any>) => void;
+    }) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const { addUtilities } = pluginApi as { addUtilities: (utilities: Record<string, any>) => void };
+      const { addUtilities } = pluginApi as {
+        addUtilities: (utilities: Record<string, any>) => void;
+      };
       addUtilities({
         ".nilor-pre": {
           "@apply ~text-lg/xl ~leading-8/9": {},
@@ -72,7 +78,7 @@ export default {
           "@apply ~text-[2.5rem]/[4rem] ~leading-[1]": {},
         },
         ".nilor-hero-text": {
-          "@apply ~text-xl/[1.75rem] ~leading-[1.75rem]/[2.5rem]": {},
+          "@apply ~text-xl/[1.43rem] ~leading-[1.75rem]/[2.rem]": {},
         },
         ".nilor-text-small": {
           "@apply ~text-base/lg ~leading-7/8": {},

9b5d0d8afb33dd84ccb9239d7e9ddd617c47f43c
Stephen <>
2025-01-17 11:36:33 +0800

    modify landing page bottom text

--- src/components/NilorLabs/index.tsx
+++ src/components/NilorLabs/index.tsx
@@ -5,22 +5,20 @@ import PreTitle from "../PreTitle";
 function NilorLabs() {
   return (
     <AnimatedSection>
-
-
-    <div className="w-full flex flex-col justify-center items-center">
-     <PreTitle pre="Nilor Labs" title="Always State-of-the-art"/>
-      <video
-        src={`https://pub-05f59bf6323d4ee3a29185a0d7e91752.r2.dev/labs.mp4`}
-        autoPlay={true}
-        loop
-        controls={false}
-        muted
-        preload="auto"
-        className="h-full w-full max-h-[800px]"
-        disablePictureInPicture
-      ></video>
-     <NilorButton text="View Services" linkTo={'/services'}/>
-    </div>
+      <div className="w-full flex flex-col justify-center items-center">
+        <PreTitle pre="services" title="Innovation across the board" />
+        <video
+          src={`https://pub-05f59bf6323d4ee3a29185a0d7e91752.r2.dev/labs.mp4`}
+          autoPlay={true}
+          loop
+          controls={false}
+          muted
+          preload="auto"
+          className="h-full w-full max-h-[800px]"
+          disablePictureInPicture
+        ></video>
+        <NilorButton text="View Services" linkTo={"/services"} />
+      </div>
     </AnimatedSection>
   );
 }
